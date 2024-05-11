9fd2ebc165a8e37bcdac65bb130c873f5a361670
mahedi066 <imam25349@gmail.com>
2024-05-11 11:35:24 +0600

    server sucessfully connected with mongodb

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k4th77t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const prot = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -14,6 +16,32 @@ app.use(
 );
 
 
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+     
+        const queriesCollection=client.db('queriesDB').collection('queries')
+
+
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+     
+    }
+  }
+  run().catch(console.dir);
 
 app.get("/", (rq, res) => {
   res.send("allprodinfo server is running ");
