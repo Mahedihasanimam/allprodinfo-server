ebe2d25c6a28d96c72ac00bd444edefa19006eb2
mahedi066 <imam25349@gmail.com>
2024-05-11 11:22:30 +0600

    server running succesfully

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const app=express()
-const cors = require('cors');
-const prot=process.env.PORT || 5000;
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const prot = process.env.PORT || 5000;
 
-app.get('/',(rq,res)=>{
-    res.send("allprodinfo server is running ")
-})
-app.listen(prot,()=>{
-    console.log(`server is running on port:${prot}`)
-})
- 
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5000"],
+    credentials:true,
+    optionsSuccessStatus:200,
+  })
+);
+
+
+
+app.get("/", (rq, res) => {
+  res.send("allprodinfo server is running ");
+});
+app.listen(prot, () => {
+  console.log(`server is running on port:${prot}`);
+});
