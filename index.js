7deca00bd43daa0834f2c694fff5fc251a28e4a3
mahedi066 <imam25349@gmail.com>
2024-05-13 20:08:36 +0600

    recomendation delete funtionality add

--- index.js
+++ index.js
@@ -66,6 +66,12 @@ const client = new MongoClient(uri, {
           const result=await queriesCollection.deleteOne(query)
           res.send(result)
         })
+        app.delete('/recomendation/:id',async(req,res)=>{
+          const id=req.params.id
+          const query={_id: new ObjectId(id)}
+          const result=await queriesCollection.deleteOne(query)
+          res.send(result)
+        })
 
         // update query 
         app.patch('/update/:id',async(req,res)=>{
