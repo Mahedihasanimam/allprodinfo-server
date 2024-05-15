457340442a4dee91cec0b4f0acfc5728f0078876
mahedi066 <imam25349@gmail.com>
2024-05-15 23:16:19 +0600

    all is done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cookeParser())
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://altprodinfo.web.app"],
     credentials: true,
     optionsSuccessStatus: 200,
   })
@@ -50,6 +50,11 @@ const verifyToken=(req,res,next)=>{
   });
 
 }
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -66,11 +71,7 @@ async function run() {
         expiresIn: "7d",
       });
       res
-      .cookie('token',token,{
-        httpOnly:true,
-        secure:false,
-        sameSite:'strict'
-      })
+      .cookie('token',token,cookieOptions)
       .send({sussess:true})
     });
     app.post('/logout',(req,res)=>{
@@ -96,7 +97,6 @@ async function run() {
     // get all queryes
     app.get("/queryes", async (req, res) => {
       const data = req.body;
- 
       const result = (await queriesCollection.find().sort({currentDate: -1 }).toArray())
       res.send(result);
     });
@@ -198,7 +198,7 @@ async function run() {
       res.send(result);
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
